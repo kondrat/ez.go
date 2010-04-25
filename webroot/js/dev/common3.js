ee4720173ac9f73e948958c0fb88a641e1230f00
kondratHome <pom01@mail.ru>
2010-04-26 00:13:14 +0400

    more

--- webroot/js/dev/common3.js
+++ webroot/js/dev/common3.js
@@ -29,12 +29,57 @@ $(document).ready( function(){
 
 
 
-
-
-
-
+    $(".currentText").bind('mouseup', function() {
+      //alert('over');
+      copy_txt();
+    });
+/*
+    $(".currentText").click( function() {
+      copy_txt();
+    });
+*/
 
 
 
 		
 });
+
+
+var txt_quote="";
+
+function copy_txt() {
+// Если переменная txt_quote не пустая, то очищаем её
+   txt_quote = "";
+  if (window.getSelection) {
+     txt_quote = window.getSelection().toString();
+  } else if (document.getSelection) {
+    txt_quote = document.getSelection();                
+  } else if (document.selection) {
+    txt_quote = document.selection.createRange().text;
+  }  
+  
+  if (txt_quote !=="") {
+   alert(txt_quote);
+   }
+  
+}
+
+function paste_txt(textarea) {
+// \n - перевод на новую строку
+   if (txt_quote=="") {
+    alert("Для вставки цитаты в новое сообщение \nвыделите нужный текст и нажмите - Вставить цитату");
+    } else {
+      //document.getElementById(textarea).value += "[q]" + txt_quote + "[/q]\n";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
